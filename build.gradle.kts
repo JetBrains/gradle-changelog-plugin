dce632c093d81922141072bf8e67868f753fea23
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-02-15 10:32:58 +0100

    Remove shadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import groovy.util.Node
 import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
@@ -11,7 +10,6 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.15.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
     id("com.github.breadmoirai.github-release") version "2.2.12"
-    id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
 description = "Gradle Changelog Plugin"
@@ -24,8 +22,6 @@ repositories {
 }
 
 dependencies {
-    shadow(localGroovy())
-    shadow(gradleApi())
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.jetbrains:markdown:0.2.0")
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.15.0")
@@ -74,30 +70,5 @@ tasks {
 
     withType<Detekt>().configureEach {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
-    }
-
-    shadowJar {
-        archiveClassifier.set("")
-        dependencies { include { it.moduleName == "markdown" } }
-    }
-
-    jar {
-        enabled = false
-        dependsOn(shadowJar)
-    }
-}
-
-publishing {
-    publications.create<MavenPublication>("pluginMaven") {
-        artifact(tasks.shadowJar)
-
-        // Hack for removing the org.jetbrains:markdown:0.1.41 dependency from the generated POM file.
-        // Somehow shadowJar does not alter the dependencies list.
-        pom.withXml {
-            (asNode().depthFirst())
-                .filterIsInstance<Node>()
-                .find { it.text() == "markdown" }
-                ?.run { parent().apply { parent().remove(this) } }
-        }
     }
 }
