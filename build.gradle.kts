f9659f3a79b4f354df22137d8774ae9ed612ec8b
Jakub Chrzanowski <jakub.chrzanowski@jetbrains.com>
2021-08-12 10:16:32 +0200

    Remove `ktlint` and `detekt`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,3 @@
-import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun properties(key: String) = project.findProperty(key)?.toString()
@@ -9,8 +8,6 @@ plugins {
     id("org.jetbrains.changelog") version "1.2.1"
     id("org.jetbrains.kotlin.jvm") version "1.5.21"
     id("com.gradle.plugin-publish") version "0.15.0"
-    id("io.gitlab.arturbosch.detekt") version "1.17.1"
-    id("org.jlleitschuh.gradle.ktlint") version "10.1.0"
 }
 
 description = properties("description")
@@ -25,7 +22,6 @@ dependencies {
     implementation("org.jetbrains:markdown:0.2.4") {
         exclude(group = "org.jetbrains.kotlin")
     }
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
 }
@@ -50,27 +46,11 @@ changelog {
     version.set("${project.version}")
 }
 
-detekt {
-    config.from(file("detekt.yml"))
-    buildUponDefaultConfig = true
-    parallel = true
-
-    reports {
-        html.enabled = false
-        xml.enabled = false
-        txt.enabled = false
-    }
-}
-
 tasks {
     listOf("compileKotlin", "compileTestKotlin").forEach {
         getByName<KotlinCompile>(it) {
             kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
         }
-    }
-
-    withType<Detekt>().configureEach {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
 
     wrapper {
