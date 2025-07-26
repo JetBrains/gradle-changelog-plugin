6f8c4f4742963e3399c6d709d6708f2e7dd32d46
Jakub Chrzanowski <jakub@chrzanowski.info>
2025-07-26 10:48:05 +0200

    Downgrade `develocity` plugin to version 3.19 in `settings.gradle.kts`.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 // Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 
 plugins {
-    id("com.gradle.develocity") version("4.1")
+    id("com.gradle.develocity") version("3.19")
     id("com.autonomousapps.build-health") version("2.19.0")
     id("org.jetbrains.kotlin.jvm") version embeddedKotlinVersion apply false
 }
