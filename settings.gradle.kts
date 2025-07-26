bea73cca1a0eca542e173536f0b2bf2e2b04c69a
Jakub Chrzanowski <jakub@chrzanowski.info>
2025-07-26 10:44:30 +0200

    Update `settings.gradle.kts`: add Gradle plugins and build scan configuration

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,20 @@
-// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+// Copyright 2000-2025 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
+
+plugins {
+    id("com.gradle.develocity") version("4.1")
+    id("com.autonomousapps.build-health") version("2.19.0")
+    id("org.jetbrains.kotlin.jvm") version embeddedKotlinVersion apply false
+}
 
 rootProject.name = "gradle-changelog-plugin"
+
+val isCI = (System.getenv("CI") ?: "false").toBoolean()
+
+develocity {
+    server = "https://ge.jetbrains.com"
+
+    buildScan {
+        termsOfUseAgree = "yes"
+        publishing.onlyIf { isCI }
+    }
+}
